d882b83aa8cc66dffe76213ec4838755f3cd5cb3
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-06-17 15:30:56 +0300

    Set up the tags and authors view

--- backend/blogs/urls.py
+++ backend/blogs/urls.py
@@ -2,8 +2,20 @@ from django.urls import path
 
 from .views import (
   BlogList,
+  BlogDetail,
+  AuthorList,
+  AuthorDetail,
+  TagList,
+  TagDetail,
 )
 
 urlpatterns = [
   path('blogs/', BlogList.as_view(), name='blog-list'),
+  path('blogs/<int:id>/', BlogDetail.as_view(), name='blog-detail'),
+
+  path('authors/', AuthorList.as_view(), name='author-list'),
+  path('authors/<int:id>/', AuthorDetail.as_view(), name='author-detail'),
+
+  path('tags/', TagList.as_view(), name='tag-list'),
+  path('tags/<int:id>/', TagDetail.as_view(), name='tag-detail'),
 ]
--- backend/blogs/views.py
+++ backend/blogs/views.py
@@ -9,3 +9,28 @@ class BlogList(generics.ListCreateAPIView):
   # permission_classes = [permissions.IsAuthenticated]
   queryset = Blog.objects.all()
   serializer_class = BlogSerializer
+
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+  # permission_classes = [permissions.IsAuthenticated]
+  queryset = Blog.objects.all()
+  serializer_class = BlogSerializer
+
+class AuthorList(generics.ListCreateAPIView):
+  # permission_classes = [permissions.IsAuthenticated]
+  queryset = Author.objects.all()
+  serializer_class = AuthorSerializer
+
+class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
+  # permission_classes = [permissions.IsAuthenticated]
+  queryset = Author.objects.all()
+  serializer_class = AuthorSerializer
+
+class TagList(generics.ListCreateAPIView):
+  # permission_classes = [permissions.IsAuthenticated]
+  queryset = Tag.objects.all()
+  serializer_class = TagSerializer
+
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+  # permission_classes = [permissions.IsAuthenticated]
+  queryset = Tag.objects.all()
+  serializer_class = TagSerializer
