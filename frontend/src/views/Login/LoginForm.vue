075a2e0701be581921d99d28f6e6e5a561e49ff9
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-06-21 23:21:22 +0300

    Set up endpoints

--- frontend/src/views/Login/LoginForm.vue
+++ frontend/src/views/Login/LoginForm.vue
@@ -1,13 +1,37 @@
 <template>
-  <form action="" class="flex flex-col mt-5">
-    <input type="email" name="email" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Email">
-    <input type="password" name="password" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Password">
+  <form action="" @submit.prevent="login" class="flex flex-col mt-5">
+    <input type="email" v-model="email" name="email" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Email">
+    <input type="password" v-model="password" name="password" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Password">
     <input type="submit" class="mb-5 bg-yellow-500 px-4 rounded-sm py-3" value="Log In">
   </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      loggedIn: false,
+    }
+  },
+
+  methods: {
+    async login() {
+      const response = await axios.post('http://192.168.46.232/api/v1/token/', {
+          email: this.email,
+          password: this.password
+      })
+
+      if (response.status === 200) {
+        this.loggedIn = true;
+        this.$router.push('/')
+      }
+      
+    }
+  },
 
 }
 </script>
--- frontend/src/views/SignUp/SignUpForm.vue
+++ frontend/src/views/SignUp/SignUpForm.vue
@@ -1,17 +1,47 @@
 <template>
   <form action="" class="flex flex-col mt-5">
-    <input type="text" name="first-name" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="First Name">
-    <input type="text" name="last-name" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Last Name">
-    <input type="email" name="email" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Email">
-    <input type="password" name="password" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Password">
-    <input type="password" name="confirmPassword" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Confirm Password">
+    <input type="text" name="first-name" v-model="first_name" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="First Name">
+    <input type="text" name="last-name" v-model="last_name" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Last Name">
+    <input type="email" name="email" v-model="email" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Email">
+    <input type="password" name="password" v-model="password" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Password">
+    <input type="password" name="confirmPassword" v-model="confirmPassword" class="mb-5 bg-white/10 px-4 rounded-sm py-3" placeholder="Confirm Password">
     <input type="submit" class="mb-5 bg-yellow-500 px-4 rounded-sm py-3" value="Sign Up">
   </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+
+  methods: {
+    async signup() {
+      const response = axios.post('http://192.168.46.232/api/v1/register/', {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: this.password,
+        password2: this.confirmPassword
+      })
+
+      if (response.status === 201) {
+        this.$router.push('/login')
+      }
+    }
+  }
+
+  beforeMount() {
+    
+  }
 }
 </script>
 
