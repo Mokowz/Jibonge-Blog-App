c383f94b0ca37b68b1362dcabb84cfa47ee23035
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-01 11:47:43 +0300

    Update db

--- backend/jibonge/settings.py
+++ backend/jibonge/settings.py
@@ -85,29 +85,43 @@ WSGI_APPLICATION = 'jibonge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     },
+
+#     'deploy': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': getenv('PGDATABASE'),
+#         'USER': getenv('PGUSER'),
+#         'PASSWORD': getenv('PGPASSWORD'),
+#         'HOST': getenv('PGHOST'),
+#         'PORT': getenv('PGPORT', 5432),
+#         'OPTIONS': {
+#         'sslmode': 'require',
+#         },
+#     }
+# }
+
+# Replace the DATABASES section of your settings.py with this
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': getenv('PGDATABASE'),
+    'USER': getenv('PGUSER'),
+    'PASSWORD': getenv('PGPASSWORD'),
+    'HOST': getenv('PGHOST'),
+    'PORT': getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
     },
-
-    'deploy': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': getenv('PGDATABASE'),
-        'USER': getenv('PGUSER'),
-        'PASSWORD': getenv('PGPASSWORD'),
-        'HOST': getenv('PGHOST'),
-        'PORT': getenv('PGPORT', 5432),
-        'OPTIONS': {
-        'sslmode': 'require',
-        },
-    }
+  }
 }
-
 
 
 # Password validation
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const apiBaseUrl = "https://alx.ronnykerosi.tech/api/v1/"
+// export const apiBaseUrl = "https://alx.ronnykerosi.tech/api/v1/"
+export const apiBaseUrl = "https://3.95.252.186/api/v1/"
