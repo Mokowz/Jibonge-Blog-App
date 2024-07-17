455d9f3be95c47b6711b69d658c4fa3518fae61f
Ronny Kerosi <ronniemokaya30@gmail.com>
2024-07-17 12:04:18 +0300

    Update deploy.sh to include the virtual environment dependency

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 # Deploy the application to the server 
 # Run this script and install all dependencies
+# Install python and its dependencies
+sudo apt install -y python3 python3-pip
+sudo pip3 install virtualenv
 
 cd Jibonge-Blog-App
 
 # Pull changes made
 git pull origin main
 
+# Activate the virtualenv
+source venv/bin/activate
+
 # Install dependencies
 pip install -r requirements.txt
+
+# Deactivate the virtual env
+deactivate
 
 # Start the gunicorn and nginx server
 sudo systemctl restart gunicorn nginx
